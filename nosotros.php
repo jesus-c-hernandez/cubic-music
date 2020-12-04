53ba1c46c1a22e56c0afaab1a89f3a1bee284fa4
Jesus Saul Castañeda Hernandez <jesus.castaneda@metricamovil.com>
2020-12-04 17:59:50 -0600

    Todo PHP

---
+++ nosotros.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nosotros</title>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="icon" href="img/logo-pequeño.png">
+</head>
+
+<body>
+
+    <header class="site-header inicio-internas">
+
+        <div class="contenedor contenido-header">
+
+            <div class="barra">
+
+                <a href="/">
+                    <img src="img/logo.png" alt="Logotipo de Cubic Music">
+                </a>
+
+                <nav class="navegacion">
+                    <a href="nosotros.html">Conócenos</a>
+                    <a href="servicios.html">Servicios</a>
+                    <a href="producciones.html">Producciones</a>
+                    <a href="contacto.html">Contacto</a>
+                </nav>
+
+            </div>
+
+        </div>
+
+    </header>
+
+    <main class="contenedor seccion">
+
+        <div class="nosotros">
+
+            <div class="imagen-nosotros">
+                <img class="radio-imagen" src="img/nosotros.jpg" alt="Imagen Nosotros">
+            </div>
+
+            <div class="contenido-nosotros">
+                <h2 class="centrar-texto">Esto es Cubic Music</h2>
+                <p>
+                    La disquera de la música independiente más grande en México. Llevamos 15 años produciendo artistas y conceptos, tenemos la experiencia que necesitas para llevar tu proyecto a un siguiente nivel.
+                </p>
+
+                <div class="estadisticas">
+
+                    <h4>Crecimiento Exponencial</h4>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quisquam nemo fugiat hic sit id aliquam non laboriosam, placeat impedit nam, asperiores possimus labore qui aspernatur illo, itaque architecto saepe?
+                    </p>
+                    <img src="img/grafico-exponencial.png" alt="Estadisticas">
+
+                </div>
+            </div>
+
+        </div>
+
+    </main>
+
+    <section class="comentarios">
+
+        <h3 class="fw-300 centrar-texto">Lo que dice el mundo</h3>
+
+        <div class="comentario">
+            <blockquote>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat maiores blanditiis, alias voluptatibus fuga eaque.
+            </blockquote>
+            <p>-Empresa Chida</p>
+        </div>
+
+        <div class="comentario">
+            <blockquote>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat maiores blanditiis, alias voluptatibus fuga eaque.
+            </blockquote>
+            <p>-Empresa Chida</p>
+        </div>
+
+        <div class="comentario">
+            <blockquote>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat maiores blanditiis, alias voluptatibus fuga eaque.
+            </blockquote>
+            <p>-Empresa Chida</p>
+        </div>
+
+        <div class="comentario">
+            <blockquote>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat maiores blanditiis, alias voluptatibus fuga eaque.
+            </blockquote>
+            <p>-Empresa Chida</p>
+        </div>
+
+        <div class="comentario">
+            <blockquote>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat maiores blanditiis, alias voluptatibus fuga eaque.
+            </blockquote>
+            <p>-Empresa Chida</p>
+        </div>
+
+    </section>
+
+    <section class="seccion compañias">
+
+        <div class="">
+
+            <h3 class="centrar-texto fw-300">Nuestras Colaboraciones</h3>
+
+            <div class="imagen-compañias">
+
+                <div class="contenido-compañias">
+
+                    <a href="#">
+                        <img src="img/icono-apple.png" class="imagen-compañia" alt="Imagen de Apple">
+                    </a>
+
+                    <a href="#">
+                        <img src="img/icono-beats.png" class="imagen-compañia" alt="Imagen de Beats">
+                    </a>
+
+                    <a href="#">
+                        <img src="img/icono-jbl.png" class="imagen-compañia" alt="Imagen de JBL">
+                    </a>
+
+                    <a href="#">
+                        <img src="img/icono-sony.png" class="imagen-compañia" alt="Imagen de Sony Music">
+                    </a>
+
+                    <a href="#">
+                        <img src="img/icono-mtv.png" class="imagen-compañia" alt="Imagen de MTV">
+                    </a>
+
+                    <a href="#">
+                        <img src="img/icono-xiaomi.png" class="imagen-compañia" alt="Imagen de Xiaomi">
+                    </a>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <footer class="">
+
+        <div class="barra footer">
+
+            <div>
+                <a href="/">
+                    <img src="img/logo.png" alt="Icono Cubic Music">
+                </a>
+            </div>
+
+            <div class="info">
+
+                <p>Redes:</p>
+
+                <div class="redes">
+                    <a href="#">
+                        <img src="img/logotipo-facebook.png" alt="Logotipo de Facebook">
+                    </a>
+                    <a href="#">
+                        <img src="img/logotipo-twitter.png" alt="Logotipo de Twitter">
+                    </a>
+                    <a href="#">
+                        <img src="img/logotipo-instagram.jpg.png" alt="Logotipo de Instagram">
+                    </a>
+                </div>
+
+                <p>Tel: 5511246879</p>
+                <p>cubicmusic_contacto@gmail.com</p>
+                <p>Calle de la Musica #40, Torreon, Coah. 27145</p>
+            </div>
+
+            <div>
+                <p>Todos los derechos reservados &copy;</p>
+            </div>
+
+        </div>
+    </footer>
+
+</body>
+
+</html>
---
+++ servicios.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Servicios</title>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="icon" href="img/logo-pequeño.png">
+</head>
+
+<body>
+
+    <header class="site-header inicio-internas">
+
+        <div class="contenedor contenido-header">
+
+            <div class="barra">
+
+                <a href="/">
+                    <img src="img/logo.png" alt="Logotipo de Cubic Music">
+                </a>
+
+                <nav class="navegacion">
+                    <a href="nosotros.html">Conócenos</a>
+                    <a href="servicios.html">Servicios</a>
+                    <a href="producciones.html">Producciones</a>
+                    <a href="contacto.html">Contacto</a>
+                </nav>
+
+            </div>
+
+        </div>
+
+    </header>
+
+    <main class="contenedor seccion">
+
+        <div class="contenido-servicios">
+
+            <h2 class="fw-300 centrar-texto">Nuestros Planes</h2>
+
+            <div class="plan">
+
+                <div class="plan-imagen">
+                    <img src="img/imagen-solista.png" alt="Imagen Solista">
+                </div>
+
+                <div class="plan-contenido">
+
+                    <h3>Plan: Brilla tú mismo</h3>
+
+                    <div class="plan-info">
+                        <h4>Ideal para: <span>Solistas</span></h4>
+                        <p>Precio: <span>Desde $1499 MXN*</span></p>
+                    </div>
+
+                    <p>
+                        Ut vel lacinia diam. Pellentesque consequat luctus libero, in scelerisque purus congue porta. Cras at lacus massa. Donec et ex quis libero tempus tempus. Curabitur ut laoreet nibh. Ut vitae commodo nunc. Praesent tempus nibh nunc, ut ultricies ex sollicitudin
+                        ut. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent interdum, ipsum at sollicitudin rhoncus, nisi turpis dapibus lorem, sed tempor ante ex vitae nunc. Pellentesque vel justo
+                        nec magna volutpat mollis. Duis ultricies quis nulla congue iaculis. Maecenas malesuada placerat ante.
+                    </p>
+                </div>
+
+            </div>
+
+            <div class="plan">
+
+                <div class="plan-imagen">
+                    <img src="img/imagen-banda.png" alt="Imagen Solista">
+                </div>
+
+                <div class="plan-contenido">
+
+                    <h3>Plan: En grupo es mejor</h3>
+
+                    <div class="plan-info">
+                        <h4>Ideal para: <span>Bandas</span></h4>
+                        <p>Precio: <span>Desde $3499 MXN*</span></p>
+                    </div>
+
+                    <p>
+                        Fusce suscipit sed arcu vitae suscipit. Ut ac metus felis. Aliquam imperdiet in mi mollis eleifend. Cras mi ex, pretium non libero nec, pellentesque laoreet ipsum. Mauris pharetra justo sed ullamcorper dignissim. Fusce vulputate nisi augue, in varius
+                        ipsum tincidunt et. Sed maximus congue quam, rutrum malesuada lacus. Duis ornare lacus et rhoncus congue. Ut vitae sagittis libero. Curabitur nec fringilla felis, vitae placerat ligula. Fusce tincidunt sit amet arcu ut cursus.
+                        Duis quis enim a augue egestas elementum vitae ac nibh. Vivamus dignissim ullamcorper tincidunt. Nullam euismod mi nisi, quis pharetra velit aliquam nec. Donec nec ornare arcu.
+                    </p>
+                </div>
+
+            </div>
+
+            <div class="plan">
+
+                <div class="plan-imagen">
+                    <img src="img/imagen-instrumental.png" alt="Imagen Solista">
+                </div>
+
+                <div class="plan-contenido">
+
+                    <h3>Plan: Instrumenta tus sueños</h3>
+
+                    <div class="plan-info">
+                        <h4>Ideal para: <span>Instrumentales</span></h4>
+                        <p>Precio: <span>Desde $999 MXN*</span></p>
+                    </div>
+
+                    <p>
+                        Nam posuere, ex nec tempus porta, dui leo pharetra augue, vel elementum elit justo vitae massa. Praesent pellentesque, eros at egestas efficitur, ligula urna consectetur erat, a ultrices ligula tortor nec ligula. Etiam luctus lobortis rutrum. Pellentesque
+                        maximus ligula nec libero sodales, et venenatis lectus ornare. Proin porttitor maximus sapien eu molestie. Sed non condimentum sem, quis elementum sem. Cras sed massa velit. Quisque gravida purus elementum, facilisis nisl in, sollicitudin
+                        nisi. Duis sit amet iaculis dolor, eu cursus neque. Mauris eu vehicula ligula. Morbi euismod ex et lacinia congue. Praesent vitae ante non ex pulvinar ullamcorper. Nam ultricies urna vitae arcu commodo dignissim at nec arcu. Sed
+                        blandit sit amet urna porta varius.
+                    </p>
+
+                </div>
+
+            </div>
+
+            <p class="texto-derecha">*Contáctanos para más información</p>
+
+            <div>
+                <a href="iniciar-sesion.php" class="boton boton-azul d-block">Comprar</a>
+            </div>
+
+        </div>
+
+    </main>
+
+    <footer class="">
+
+        <div class="barra footer">
+
+            <div>
+                <a href="/">
+                    <img src="img/logo.png" alt="Icono Cubic Music">
+                </a>
+            </div>
+
+            <div class="info">
+
+                <p>Redes:</p>
+
+                <div class="redes">
+                    <a href="#">
+                        <img src="img/logotipo-facebook.png" alt="Logotipo de Facebook">
+                    </a>
+                    <a href="#">
+                        <img src="img/logotipo-twitter.png" alt="Logotipo de Twitter">
+                    </a>
+                    <a href="#">
+                        <img src="img/logotipo-instagram.jpg.png" alt="Logotipo de Instagram">
+                    </a>
+                </div>
+
+                <p>Tel: 5511246879</p>
+                <p>cubicmusic_contacto@gmail.com</p>
+                <p>Calle de la Musica #40, Torreon, Coah. 27145</p>
+            </div>
+
+            <div>
+                <p>Todos los derechos reservados &copy;</p>
+            </div>
+
+        </div>
+    </footer>
+
+</body>
+
+</html>
